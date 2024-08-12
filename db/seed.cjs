010931e9f8eba1cb4622f05ff6d1ed819a3953f0
JonnelleC <carrjonnelle@gmail.com>
2024-08-12 15:26:07 -0400

    back end setup

--- db/seed.cjs
+++ db/seed.cjs
@@ -3,9 +3,10 @@ const client = require("./client.cjs");
 const dropTables = async () => {
     try{
         await client.query(`
+            DROP TABLE IF EXISTS cart;
             DROP TABLE IF EXISTS bags;
-            DROP TABLE IF EXISTS sellers;
-            
+            DROP TABLE IF EXISTS customers;
+          
         
          `);
     } catch (err) {
@@ -17,7 +18,7 @@ const dropTables = async () => {
 const createCustomerTable = async () => {
     try {
       await client.query(`
-              CREATE TABLE IF NOT EXISTS customer (
+              CREATE TABLE IF NOT EXISTS customers (
                   username VARCHAR(50) PRIMARY KEY,
                   emailaddress VARCHAR(100) NOT NULL,
                   firstname VARCHAR(50) NOT NULL,
@@ -42,19 +43,39 @@ const createCustomerTable = async () => {
             brand VARCHAR(50) NOT NULL,
             release_date INT,
             description TEXT,
-            price Real NOT NULL)
+            price Real NOT NULL);
             
-            `)
+            `);
     } catch (err){
         console.log(err);
   }
   };
 
+  const createCartTable = async () => {
+    try{
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS cart(
+             id SERIAL PRIMARY KEY,
+             price_total Real NOT NULL, 
+             is_open BOOLEAN NOT NULL,
+             customer_id INT REFERENCES customers(id),
+             bag_id INT REFERENCES bags(id)
+            );
+            `);
+    } catch(err) {
+        console.log(err);
+    }
+  }
+
 
 
   const seedData = async () => {
     await client.connect();
+    await dropTables();
     await createBagTable();
     await createCustomerTable();
+    await createCartTable();
     await client.end();
   }
+
+  seedData();
