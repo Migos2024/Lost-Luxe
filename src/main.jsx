a8d7efe2453a6611f8f794e59f40f6cde0db1dd3
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-08-13 18:03:24 -0400

    Enable routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-
-    <App />
- 
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<HashRouter><App /></HashRouter>)
