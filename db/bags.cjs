2a61fe7e9995cbd3ac240bc23234450dc79fbb14
Antonio Ramirez <antonioramirez9614@gmail.com>
2024-08-14 15:58:58 -0500

    finished defining first 10 bags, the last 10 are placeholders. + fixed errors involving getSingleBag,getAllBags,createBag to get DB to seed

--- db/bags.cjs
+++ db/bags.cjs
@@ -1,15 +1,15 @@
 const client = require('./client.cjs');
 
 
-const createBag = async (bagname, brand, releasedate, description, price) => {
+const createBag = async (bag_name, brand, release_date, description, price, image_path) => {
     try{
         const bagInfo = await client.query(`
             INSERT INTO bags
             (bag_name, brand, release_date, description, price, image_path)
             VALUES ($1, $2, $3, $4, $5, $6)
-            `, [bag.bag_name, bag.brand, bag.release_date, bag.description, bag.price, imagePath]);
+            `, [bag_name, brand, release_date, description, price, image_path]);
 
-            console.log(bagInfo);
+            // console.log(bagInfo);
 
     } catch (err) {
         console.log(err);
@@ -21,19 +21,19 @@ const getAllBags = async () => {
         const { rows } = await client.query(`
             SELECT * FROM bags;
             `);
-            return rows;
+            // return rows;
     } catch (err){
         console.log(err);
     }
 }
 
-const getSingleBag = async () => {
+const getSingleBag = async (id) => {
    try{
     const oneBag = await client.query(`
         SELECT * FROM bags
-        WHERE id = $1;
+        WHERE bags.id = $1;
         `, [id]);
-        return oneBag.rows[0];
+        // return oneBag.rows[0];
    } catch (err) {
     console.log(err);
     return null;
--- db/seed.cjs
+++ db/seed.cjs
@@ -1,9 +1,7 @@
 require('dotenv').config();
-const client = require("./client.cjs");
-const {createBag} = require(`bags.cjs`);
+const client = require('./client.cjs');
+const {createBag, getSingleBag} = require('./bags.cjs');
 const path = require('path');
-
-const getSingleBag = require("./client.cjs");
 
 const dropTables = async () => {
     try{
@@ -83,7 +81,7 @@ const createCustomerTable = async () => {
         brand: 'Chanel',
         release_date: 2014,
         description: 'A stylish black leather handbag. It has lovely gold hardware to better suit your style!',
-        price: 3599.99,
+        price: 3599.27,
         image: 'black1.jpeg'
       },
       {
@@ -91,14 +89,158 @@ const createCustomerTable = async () => {
         brand: 'Chanel',
         release_date: 2017,
         description: 'A quality knit Chanel handbag with beautiful gold hardware.',
-        price: 5379.99,
+        price: 5379.83,
         image: 'black2.jpeg'
+      },
+      {
+        bag_name: 'White/Black',
+        brand: 'Chanel',
+        release_date: 2014,
+        description: 'A gorgeous leather Chanel handbag with enticing black hardware.',
+        price: 2379.69,
+        image: 'black3.jpeg'
+      },
+      {
+        bag_name: 'Blue/Lilac',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'This suede Chanel bags elevates your look.',
+        price: 2752.79,
+        image: 'blue1.jpeg'
+      },
+      {
+        bag_name: 'Blue/ Gold',
+        brand: 'Chanel',
+        release_date: 2012,
+        description: 'A stylish blue leather handbag. It has lovely gold hardware to better suit your style!',
+        price: 1379.99,
+        image: 'blue2.jpeg'
+      },
+      {
+        bag_name: 'Green/ Gold',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality leather Chanel with astounding gold hardware with leather woven-in.',
+        price: 3979.99,
+        image: 'green1.jpeg'
+      },
+      {
+        bag_name: 'Mint',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A mint Chanel handbag so fresh, your breath will feel it. This Chanel comes adorned with silver and mint hardware and mint leather woven-in.',
+        price: 4439.23,
+        image: 'green2.jpeg'
+      },
+      {
+        bag_name: 'Sorbet/ Gold',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'Bring on the summer with some Sorbet in your life. Gold hardware for class',
+        price: 2779.99,
+        image: 'orange1.jpeg'
+      },
+      {
+        bag_name: 'Sorbet/ Clutch',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'Dare to fit summer in a clutch?',
+        price: 2379.99,
+        image: 'orange2.jpeg'
+      },
+      {
+        bag_name: 'Pearl',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'Pearl Chanel handbag from 2017 Spring/ Summer collection',
+        price: 13379.99,
+        image: 'pearl1.jpeg'
+      },
+      {
+        bag_name: 'Pink Knitted',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'pink1.jpeg'
+      },
+      {
+        bag_name: 'Pink/ Gold',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'pink2.jpeg'
+      },
+      {
+        bag_name: 'Pink Box',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'pink3.jpeg'
+      },
+      {
+        bag_name: 'Pink Gold/Silver',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'pink4.jpeg'
+      },
+      {
+        bag_name: 'Peace Pink',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'pink5.jpeg'
+      },
+      {
+        bag_name: 'Deep Pink',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'pink6.jpeg'
+      },
+      {
+        bag_name: 'Purple',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'purple1.jpeg'
+      },
+      {
+        bag_name: 'Cherry',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'red1.jpeg'
+      },
+      {
+        bag_name: 'Maroon',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'red2.jpeg'
+      },
+      {
+        bag_name: 'Silver',
+        brand: 'Chanel',
+        release_date: 2017,
+        description: 'A quality knit Chanel handbag with beautiful gold hardware.',
+        price: 5379.99,
+        image: 'silver1.jpeg'
       },
     ]
 
     for(const bag of bags) {
-      const imagePath = path.join(`/chanelBags`, bags.image)
-      await createBag()
+      const imagePath = path.join('/chanelBags', bag.image)
+      await createBag(bag.bag_name, bag.brand, bag.release_date, bag.description, bag.price, imagePath)
     }
   };
 
@@ -109,10 +251,9 @@ const createCustomerTable = async () => {
     await createCustomerTable();
     await createCartTable();
     await seedBags();
-    await getSingleBag();
+    await getSingleBag(1);
     await client.end();
   }
-
 
 
   seedData();
