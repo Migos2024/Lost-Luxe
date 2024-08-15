3b5bb6bb1421f745a4d5d6bced782975c53349f1
Sandough <jose.sndov@gmail.com>
2024-08-15 02:26:55 -0500

    added login html

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,18 +1,37 @@
+import React from "react";
+import { useState } from "react";
+
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <h1>This is the log in page</h1>
-  )
-}
+    <>
+      <h1>This is the log in page</h1>;
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="username"
+          id="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="pw"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log In</button>
+      </form>
+      ;
+    </>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-export default Auth
+export default Auth;
